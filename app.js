3f5d256840ae3966f3ba084b3a32b6fd90502071
amenov <amenov.abdulsalam@gmail.com>
2023-10-27 12:04:16 +0600

    add morgan

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const morgan = require('morgan')
 const cors = require('cors')
 const rateLimit = require('express-rate-limit')
 
@@ -6,6 +7,7 @@ const config = require('./config')
 
 const app = express()
 
+app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
@@ -16,3 +18,9 @@ void [
   require('./modules/auth/auth.controller')
   // ...
 ].forEach((moduleController) => app.use(...moduleController))
+
+const { port, host } = config.server
+
+app.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`)
+})
