93ff12d8cfda99b687b553d9dc41fcc0b0c437dd
amenov <amenov.abdulsalam@gmail.com>
2023-10-27 13:06:01 +0600

    add comments

--- app.js
+++ app.js
@@ -8,16 +8,33 @@ const printServerInfo = require('./utils/print-server-info')
 
 const app = express()
 
+// Registering middleware
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+// app.use(middleware)
+// ...
 
-if (config.server.cors) app.use(cors(config.server.cors))
-if (config.server.rateLimit) app.use(rateLimit(config.server.rateLimit))
+// Enable CORS if configured
+if (config.server.cors) {
+  app.use(cors(config.server.cors))
+}
 
-void [
-  require('./modules/auth/auth.controller')
+// Enable rate limiting if configured
+if (config.server.rateLimit) {
+  app.use(rateLimit(config.server.rateLimit))
+}
+
+// Registering shared services
+global.$sharedServices = {
+  authService: require('./modules/auth/auth.service')
+  // service_name: require('./modules/service_name/service_name.service')
   // ...
-].forEach((moduleController) => app.use(...moduleController))
+}
+
+// Registering module controllers
+app.use(...require('./modules/auth/auth.controller'))
+// app.use(...require('./modules/module_name/module_name.controller'))
+// ...
 
 app.listen(config.server.port, config.server.host, printServerInfo(config))
